89c3b6f9a8630c25cbe8a070d76a99bd4829e87b
evgenyfokin <fokin_e@inbox.ru>
2023-07-19 13:08:14 +0300

    fix

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,10 +1,15 @@
-// import axios from "axios";
+import axios from "axios";
 //
 // const API_URL = process.env.REACT_APP_API_URL
 // console.log(API_URL)
 // export const register = (userData) => axios.post(`${API_URL}/auth/register`, userData)
 // export const login = (userData) => axios.post(`${API_URL}/auth/login`, userData)
-import axios from "axios";
+//
+//
+// export const register = (userData) => axios.post(`/api/auth/register`, userData);
+// export const login = (userData) => axios.post(`/api/auth/login`, userData);
 
-export const register = (userData) => axios.post(`/api/auth/register`, userData);
-export const login = (userData) => axios.post(`/api/auth/login`, userData);
+const API_URL = process.env.REACT_APP_API_URL
+console.log(API_URL)
+export const register = (userData) => axios.post(`${API_URL}/api/auth/register`, userData)
+export const login = (userData) => axios.post(`${API_URL}/api/auth/login`, userData)
