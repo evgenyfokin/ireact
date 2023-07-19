003bf0b7632a281c256d7c9c1cbedc4b0eda8bb5
evgenyfokin <fokin_e@inbox.ru>
2023-07-19 11:26:21 +0300

    fix

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = 'http://localhost:3004'
+const API_URL = process.env.REACT_APP_API_URL
+console.log(API_URL)
 // console.log(import.meta.env.VITE_APP_API_URL)
 
 export const register = (userData) => axios.post(`${API_URL}/auth/register`, userData)
